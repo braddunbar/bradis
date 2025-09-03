6e58e3c6a208d670bba1343bff1c1a8f311d35ae
Brad Dunbar <dunbarb2@gmail.com>
2025-09-03 00:35:03 -0400

    new lints in Cargo.toml

--- ci/check.sh
+++ ci/check.sh
@@ -3,19 +3,5 @@
 set -ex
 
 cargo fmt --all -- --check
-cargo clippy --locked --all --tests -- \
-  -D clippy::all \
-  -D clippy::dbg_macro \
-  -A clippy::enum_glob_use \
-  -A clippy::module_name_repetitions \
-  -A clippy::missing_errors_doc \
-  -A clippy::too_many_lines \
-  -D warnings
-cargo clippy --no-default-features --locked --all --tests -- \
-  -D clippy::all \
-  -D clippy::dbg_macro \
-  -A clippy::enum_glob_use \
-  -A clippy::module_name_repetitions \
-  -A clippy::missing_errors_doc \
-  -A clippy::too_many_lines \
-  -D warnings
+cargo clippy --locked --all --tests -- -D warnings
+cargo clippy --no-default-features --locked --all --tests -- -D warnings
